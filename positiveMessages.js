4fb8a21cc5db1812a955c7cc258f4481848a0b46
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2021-07-13 21:53:03 +0200

    exploring repl env

---
+++ positiveMessages.js
@@ -0,0 +1,15 @@
+let messages=[ "A change of environment can be a good thing!",
+              "You will make it!",
+                            "Just run with the code!"
+                            
+                            ]
+let messages=[ A change of environment can be a good thing!,
+              You will make it!
+                            Just run with the code!
+                            
+                            ];
+let messages=[ "A change of environment can be a good thing!",
+              "You will make it!",
+                            "Just run with the code!"
+                            
+messages.forEach(message=> console.log(message))
